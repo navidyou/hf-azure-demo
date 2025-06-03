496b052eefe58905f14d3c931dd97da077e7f334
unknown <n.yousefabadi@gmail.com>
2025-06-03 11:59:46 -0700

    role back to successfull run 2

--- src/app/main.py
+++ src/app/main.py
@@ -21,7 +21,7 @@ FastAPIInstrumentor.instrument_app(app)
 
 class InferenceRequest(BaseModel):
     text: str
-#lets see hellooooo
+#lets see hellooooo helo
 @app.post("/predict")
 async def predict(req: InferenceRequest):
     start = time.perf_counter()
