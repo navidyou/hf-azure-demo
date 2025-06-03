2d743a795d4d3e773b876c71eaeb959b3c8e287d
unknown <n.yousefabadi@gmail.com>
2025-06-02 20:41:35 -0700

    replaced bicep with terraform 7

--- src/app/main.py
+++ src/app/main.py
@@ -21,7 +21,7 @@ FastAPIInstrumentor.instrument_app(app)
 
 class InferenceRequest(BaseModel):
     text: str
-#hello world of nones
+#hello world of nonese
 @app.post("/predict")
 async def predict(req: InferenceRequest):
     start = time.perf_counter()
--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,6 @@ resource "azurerm_container_app" "app" {
   name                         = "sentiment-api-${var.stage}"
   container_app_environment_id = azurerm_container_app_environment.env.id
   resource_group_name          = var.resource_group_name
-  location                     = var.location
   revision_mode                = "Multiple"
 
   template {
