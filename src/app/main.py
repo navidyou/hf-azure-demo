30f643c88a0134b5d3a9d1c5c3087d795a781b0c
unknown <n.yousefabadi@gmail.com>
2025-06-02 21:57:29 -0700

    fix stage env already exists

--- src/app/main.py
+++ src/app/main.py
@@ -21,7 +21,7 @@ FastAPIInstrumentor.instrument_app(app)
 
 class InferenceRequest(BaseModel):
     text: str
-#hello world of nonese ugh 6
+#hello world of nonese ugh 7
 @app.post("/predict")
 async def predict(req: InferenceRequest):
     start = time.perf_counter()
