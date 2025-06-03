c5aa6e258b353e67eecdfafc5d96130e3e1334d8
unknown <n.yousefabadi@gmail.com>
2025-06-02 20:02:19 -0700

    replaced bicep with terraform 2

--- src/app/main.py
+++ src/app/main.py
@@ -21,7 +21,7 @@ FastAPIInstrumentor.instrument_app(app)
 
 class InferenceRequest(BaseModel):
     text: str
-#hello world of
+#hello world of n
 @app.post("/predict")
 async def predict(req: InferenceRequest):
     start = time.perf_counter()
--- terraform/main.tf
+++ terraform/main.tf
@@ -4,11 +4,6 @@ provider "azurerm" {
   features {}
 }
 
-variable "location" {}
-variable "resource_group_name" {}
-variable "acr_name" {}
-variable "image_tag" {}
-variable "stage" {}
 
 # ───── Get existing ACR ─────
 data "azurerm_container_registry" "acr" {
