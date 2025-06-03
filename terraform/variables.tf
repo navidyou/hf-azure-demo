8f2918a3618a5dc79f69b731e22591a1a913ee8b
unknown <n.yousefabadi@gmail.com>
2025-06-03 11:32:11 -0700

    missing vars in tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,6 +6,9 @@ variable "stage" {}
 variable "acr_resource_group_name" {
   description = "RG that actually contains the ACR (can differ from stage/prod RGs)"
 }
+variable "existing_env_name" {}
+variable "existing_env_rg_name" {}
+
 locals {
   is_prod = var.stage == "prod"
   container_app_env_id = local.is_prod ? data.azurerm_container_app_environment.existing[0].id : azurerm_container_app_environment.env[0].id
