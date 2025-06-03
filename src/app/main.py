f351188588cddfb0cfeff7c940ab31fc1b53057d
unknown <n.yousefabadi@gmail.com>
2025-06-02 21:20:37 -0700

    replaced bicep with terraform 11

--- src/app/main.py
+++ src/app/main.py
@@ -21,7 +21,7 @@ FastAPIInstrumentor.instrument_app(app)
 
 class InferenceRequest(BaseModel):
     text: str
-#hello world of nonese ugh 4
+#hello world of nonese ugh 5
 @app.post("/predict")
 async def predict(req: InferenceRequest):
     start = time.perf_counter()
