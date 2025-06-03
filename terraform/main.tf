a1339d6b9247be973ab1889a8d77b2fb8cd33a4e
unknown <n.yousefabadi@gmail.com>
2025-06-02 21:40:53 -0700

    replaced bicep with terraform 12

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 # ───── Get existing ACR ─────
 data "azurerm_container_registry" "acr" {
   name                = var.acr_name
-  resource_group_name = var.resource_group_name
+  resource_group_name = var.acr_resource_group_name
 }
 
 # ───── Log Analytics Workspace ─────
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,6 @@ variable "resource_group_name" {}
 variable "acr_name" {}
 variable "image_tag" {}
 variable "stage" {}
+variable "acr_resource_group_name" {
+  description = "RG that actually contains the ACR (can differ from stage/prod RGs)"
+}
